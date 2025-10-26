d1984cb22c945b1d9497a0354eba5e5dedad2683
Karthik Suresh <karthiksuresha2012@gmail.com>
2025-10-25 17:54:34 -0700

    lm arch defined and working

--- dataset/text_dataset.py
+++ dataset/text_dataset.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Dict, Tuple
+from typing import Tuple
 import logging
 import math
 import numpy as np
@@ -8,9 +8,9 @@ from torch.utils.data import Dataset
 
 from tokenizer.bpe import BPETokenizer
 
-class TextDataTokenizer(Dataset):
+class TinyStories(Dataset):
     """
-    Pytorch dataset class to load a saved tokenized data file (.npy).
+    Pytorch dataset class to load a saved tokenized data file (np memmap) for Tiny stories.
     """
     def __init__(self, np_file_path:str, split:str="train", seq_len:int=512, pad_token_id:int=0):
         self.split = split
@@ -24,23 +24,28 @@ class TextDataTokenizer(Dataset):
         else:
             raise ValueError("split has to be one of [train, val], got: ", split)
         self.data_len = len(self.data)
-        logging.info(f"loaded {split } with {self.data_len} tokens")
-        
+        logging.info(f"Loaded {split} split with {self.data_len:,} tokens")
+
     def __len__(self):
         #ceil since we don't want to skip the last index in the data
         return math.ceil(self.data_len / self.seq_len)
     
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
         start = idx*self.seq_len
         end = min(start + self.seq_len, self.data_len)
         seq = self.data[start:end]
         
         #pad in case the length of seq is < seq_len
         if len(seq) < self.seq_len:
-            pad = np.zeros(self.seq_len - len(seq), self.pad_token_id, dtype=np.uint16)
+            pad = np.zeros(self.seq_len - len(seq), dtype=np.uint16)
+            pad[:] = self.pad_token_id
             seq = np.concatenate([seq, pad])
         
         #TODO: benchmark moving to GPU and upcasting to long vs upcasting first.
-        return torch.from_numpy(seq.astype(np.int64)) 
+        inp = torch.from_numpy(seq[:-1].astype(np.int64))
+        label = torch.from_numpy(seq[1:].astype(np.int64))
+
+        return inp, label
+            
     
         
--- model/llm.py
+++ model/llm.py
@@ -1,8 +1,33 @@
-from typing import Tuple, Dict
-import torch
-import torch.nn as nn
-from torch.nn
+from typing import Tuple
+import torch #type: ignore
+import torch.nn as nn # type: ignore
 
+
+
+class RoPE(nn.Module):
+    """
+    q = [q1, q2, ... qs]
+    -> [[q1, q2], [q3, q4], ...[qs-1, qs]]
+    -> let x = [x1, x2]; rotation R(x) = [[cost, -sint], [sint, cost]] @ x = e^it*(x1 + ix2) 
+    """
+    def __init__(self, seq_len:int, head_dim:int):
+        super().__init__()
+        self.d = head_dim
+        self.d_half = head_dim // 2
+        pos = torch.arange(0,seq_len) #(s,)
+        freqs = 1/torch.pow(10000, torch.arange(0,self.d_half,2)/self.d) # 10000 ^ (-2*i/d) (d/2,)
+        thetas = torch.einsum('s,d->sd',pos, freqs)
+        thetas = torch.polar(torch.ones_like(thetas), thetas) # convert to polar with mag 1 and angle theta = e^it
+        self.register_buffer("thetas", thetas) # (s, d/2)
+    
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        b,h,s,d = x.shape
+        x = x.float().view(b, h, s, self.d_half, 2) #( b, h, s, d/2, 2)
+        x = torch.view_as_complex(x) #(b, h, s, d/2)
+        x = torch.einsum('bhsd,sd->bhsd', x, self.thetas) #TODO: if s < self.seq_len?
+        x = torch.view_as_real(x).reshape(b, h, s, d) #(b, h, s, d/2, 2) -> (b, h, s, d)
+
+        return x #(b, h, s, d)
 
 class MultiHeadAttention(nn.Module):
     """
@@ -16,10 +41,10 @@ class MultiHeadAttention(nn.Module):
         self.register_buffer("causal_mask", #so that its part of the model state dict and is automatically placed on device
                                                 torch.tril(torch.ones(seq_len, seq_len)).unsqueeze(0).unsqueeze(0)
                                                 ) #(1, 1, S, S)
-        self.d_half = d_model // 2
+        self.rope = RoPE(seq_len=seq_len, head_dim=d_model // num_heads)
 
     def _get_qkv(self, x:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-         #x.shape -> (B, S, D)
+        #x.shape -> (B, S, D)
         x = self.w_qkv(x)  #(x.shape -> (B, S, 3*D))
         q, k, v = torch.chunk(x, chunks=3, dim=-1) #(B, S, D))
         B, S, D = q.shape
@@ -29,36 +54,13 @@ class MultiHeadAttention(nn.Module):
         q = q.view(B, S, self.num_heads, D // self.num_heads).transpose(1,2) # (B, H, S, d)
         k = k.view(B, S, self.num_heads, D // self.num_heads).transpose(1,2) # (B, H, S, d)
         v = v.view(B, S, self.num_heads, D // self.num_heads).transpose(1, 2) # (B, H, S, d)
+
         return q, k, v
-    
-    def _get_rope(self, x:torch.Tensor) -> torch.Tensor:
-        """
-        q = [q1, q2, ... qs]
-        -> [[q1, q2], [q3, q4], ...[qs-1, qs]]
-        -> let x = [x1, x2]; rotation R(x) = [[cost, -sint], [sint, cost]] @ x = e^it*(x1 + ix2) 
-        """
-        b, h, s, d = x.shape
-
-        #convert x to complex number
-        x = x.float().reshape(b, h, s, self.d_half, 2)
-        x_complex = torch.view_as_complex(x)   # (b, h, s, d_half)
-
-        #pos and thetas
-        pos = torch.arange(s, device=x.device) #(s,)
-        freqs = 1/torch.pow(10000, torch.arange(0, self.d_half, device=x.device)/self.d_half) #(d_half,)
-        thetas = torch.einsum('s,d->sd',pos,freqs) #outer product thetas = (s,d_half)
-        rot = torch.polar(torch.ones_like(thetas), thetas) #mag = 1, angle = thetas (s,d_half)
-
-        #rotate x
-        x = x_complex * rot[None, None, :, :] #(b, h, s, d_half) * (1,1,s,d_half) (element wise multiply)
-        x = torch.view_as_real(x).reshape(b, h, s, d)
-
-        return x #(b,h,s,d)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        q, k, v = self._get_qkv(x)
-        q = self._get_rope(q)
-        k = self._get_rope(k)
+        q, k, v = self._get_qkv(x) #(b, h, s, d)
+        q = self.rope(q) #(b, h, s, d)
+        k = self.rope(k) #(b, h, s, d)
 
         attn_scores = torch.einsum('bhqd,bhkd->bhqk', q, k)
         # attn_scores = attn_scores/q.shape[-1]**0.5 #(b, h, q, k)
@@ -80,18 +82,18 @@ class LayerNorm(nn.Module):
     x = (x - mean)/(sqrt(var) + eps)
     x = gamma*x + beta (rescale and shift)
     """
-    def __init__(self, model_dim:int):
+    def __init__(self, d_model:int):
         super().__init__()
-        self.gamma = nn.Parameter(torch.ones(model_dim))
-        self.beta = nn.Parameter(torch.zeros(model_dim))
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
         self.eps = 1e-6
     
     def forward(self, x):
         # x.shape (b,s,d)
-        mean = torch.mean(x, dim=-1, keep_dim=True) #(b, s)
+        mean = torch.mean(x, dim=-1, keepdim=True) #(b, s)
         #(b, s) (unbiased=False -> var = 1/N*sum(x_i - mean)**2) and not 1/N-1*()
         #apparently has smoother gradients.
-        var = torch.var(x, dim=-1, keep_dim=True, unbiased=False) 
+        var = torch.var(x, dim=-1, keepdim=True, unbiased=False) 
         x = (x - mean)/(torch.sqrt(var) + self.eps)
         x = self.gamma*x + self.beta
 
@@ -99,15 +101,17 @@ class LayerNorm(nn.Module):
 
 class TransformerBlock(nn.Module):
     """
-    The class which defines the small language model to be trained on tiny stories dataset.
+    The class which defines the Transformer block (mha with rope, ffn, and ln).
     """
 
     def __init__(self, vocab_size: int, d_model:int, num_heads:int, seq_len:int) -> None:
         super().__init__()
 
         self.num_heads = num_heads
+
         self.token_emb = nn.Embedding(vocab_size, d_model)
         self.mha = MultiHeadAttention(d_model=d_model, num_heads=num_heads, seq_len=seq_len)
+
         self.ffn = nn.Sequential(
             nn.Linear(d_model, 4*d_model),
             nn.GELU(),
@@ -116,21 +120,81 @@ class TransformerBlock(nn.Module):
         self.ln1 = LayerNorm(d_model)
         self.ln2 = LayerNorm(d_model)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        #x.shape -> (b, s)
-        #1. embed the tokens
-        x = self.token_emb(x) # x.shape -> (b, s, d_model)
-        
-        #2. layer norm before mha? some archs have that. for v1, let's stick with layernorm after mha
-        #...
-        
-        #3. MHA + RoPE + residual + ln1
+    def forward(self, x: torch.Tensor) -> torch.Tensor:        
+        # MHA + RoPE + residual + ln1
         x = x + self.mha(self.ln1(x)) # x.shape -> (b, s, d_model)
 
-        #5. ffn + residual + ln2
+        # ffn + residual + ln2
         x = x + self.ffn(self.ln2(x)) # x.shape -> (b, s, d_model)
 
         return x
+    
+class TransformerLM(nn.Module):
+    """
+    The entry point to define the language model object which combines:
+    1. embedding
+    2. transformer block (mha+ rope, ffn, ln)
+    3. final ffn + softmax over the vocab
+
+    Forward:
+    x: (batch, seq_len) token indices
+    Returns:
+    logits: (batch, seq_len, vocab_size) raw scores before softmax
+
+    """
+
+    def __init__(self,vocab_size: int, d_model:int, num_heads:int, seq_len:int, n_layers:int):
+        super().__init__()
+        self.n_layers = n_layers
+        self.token_emb = nn.Embedding(vocab_size, d_model)
+
+        self.transformer_blocks = nn.ModuleList([
+            TransformerBlock(vocab_size, d_model, num_heads, seq_len) for _ in range(n_layers)
+            ]
+        )
+        self.ln = LayerNorm(d_model)
+        self.lm_head = nn.Linear(d_model, vocab_size, bias=False)
+        #[optional] weight tying: to reduce num params
+        self.lm_head.weight = self.token_emb.weight
+    
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        #embed tokens: (b, s) -> (b, s, d)
+        x = self.token_emb(x)
+        # print("x.shape after emb: ", x.shape)
+        #mha with rope + ln + ffn + ln: (b, s, d) -> (b, s, d)
+        for block in self.transformer_blocks:
+            x = block(x)
+            # print("x.shape after mha: ", x.shape)
+
+        #ln + final ffn
+        x = self.ln(x)
+        x = self.lm_head(x) 
+
+        return x
+    
+
+if __name__ == "__main__":
+    b = 1
+    s = 12
+    num_heads = 4
+    vocab_size = 16
+    d_model = 8
+    n_layers = 2
+
+    test_llm = TransformerLM(
+        vocab_size=vocab_size,
+        d_model=d_model,
+        num_heads=num_heads,
+        seq_len=s,
+        n_layers=n_layers
+    )
+
+    x = torch.randint(0, vocab_size, size=(b,s))
+    print(x.shape)
+
+    print(test_llm(x).shape)
+
+
 
 
 
